aaa10b279b4de37f12731d1d313bc8f1219385ee
Giancarlo Villanueva <villanueva.giancarlo@gmail.com>
2016-05-30 20:56:57 -0700

    Begin scaling checkers board size

--- setup.js
+++ setup.js
@@ -1,24 +1,46 @@
 /**
- * Created by cvillanueva on 5/17/2016.
+ * @file Defines the extension points for checkers system
+ * @author Giancarlo Villanueva
  */
 var Man = require("./man");
 var King = require("./king.js");
 
+// Chessboard size constants
+const GRID_SIZE = 2.25;
+const GRID_ROWS = 8;
+const GRID_COLS = 8;
+const GRID_SCALE = 5;
+const GRID_SIZE_SCALED = GRID_SIZE * GRID_SCALE;
+
 module.exports = {
+    /**
+     *
+     * @param users
+     */
     preStart: function(users) {
-        if (users.length != 2)
-            
-    }
+    },
+
+    /**
+     * Sets up the units required for a game of checkers.
+     * @returns {Object} Collection of  Unit-derived objects
+     */
     setup: function() {
+        var bounds = {};
         var units = {};
+
+        // Set up light units
         for (var r = 0; r < 3; r++)
             for (var m = 0; m < 4; m++)
                 units[4 * r + m] = new Man(m * 2 + r % 2, r, 0);
 
+        // Set up dark units
         for (var r = 5; r < 8; r++)
             for (var m = 0; m < 4; m++)
                 units[4 * r + m] = new Man(m * 2 + r % 2, r, 0);
 
-        return units;
+        return {
+            bounds: { x: 0, y: 0, w: GRID_SIZE_SCALED * GRID_COLS, h: GRID_SIZE_SCALED * GRID_ROWS },
+            units: units
+        };
     }
 }
