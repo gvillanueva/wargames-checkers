8c3a1d07999c318276e4f900f345cf7fcf63bf9a
Giancarlo Villanueva <cvillanueva@graphicproducts.com>
2016-05-31 14:57:46 -0700

    Fix Man's class inheritance/calling
    A little push towards correcting validation of unit movement.

--- man.js
+++ man.js
@@ -2,17 +2,15 @@
  * @file Defines the behavior of the Man in checkers
  * @author Giancarlo Villanueva
  */
-/**
- * Created by cvillanueva on 5/17/2016.
- */
 var WargamesUnit = require("../wargames/unit.js");
 var WargamesError = require("../wargames/error.js");
 
-Man.prototype = Object.create(WargamesUnit);
+Man.prototype = Object.create(WargamesUnit.prototype);
 Man.prototype.constructor = Man;
 
-function Man(x,y,z,image) {
+function Man(x,y,z,dir,image) {
     WargamesUnit.prototype.constructor.call(this,x,y,z,image);
+    this.dir = dir;
 }
 
 /**
@@ -22,10 +20,13 @@ function Man(x,y,z,image) {
  * @param z Z-coordinate of unit's destination.
  */
 Man.prototype.move = function(x,y,z) {
-    WargamesUnit.prototype.move.call(this,x,y,z);
     console.log("Man.move()");
     if (x == this.x || y == this.y)
         throw new WargamesError("Invalid move: man must move diagonally");
+    if ((this.y - y < 0) == (this.dir < 0))
+        throw new WargamesError("Invalid move: wrong direction");
+
+    WargamesUnit.prototype.move.call(this,x,y,z);
 }
 
 module.exports = Man;
